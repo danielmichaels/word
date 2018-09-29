0fe8e30caa5a9c150206bb5679099725b9664d12
Dan <dans.address@outlook.com>
2018-09-29 20:25:02 +0000

    output.py added thesaurus fn that prints synonym and antonym to term.

--- word/output.py
+++ word/output.py
@@ -1,3 +1,5 @@
+from json import JSONDecodeError
+
 import click
 
 
@@ -23,9 +25,43 @@ def print_definition(json_):
 
 def etymology(json_):
     try:
-        for etymology in json_['etymologies']:
-            return etymology
+        for origin in json_['etymologies']:
+            return origin
         else:
             return None
     except KeyError:
         pass
+
+
+def thesaurus(json_, thesaurus_: str):
+    try:
+        thesaurus_words = list()
+        for like_word in json_[thesaurus_]:
+            word = like_word['text'].capitalize()
+            thesaurus_words.append(word)
+        return print_lists(thesaurus_words)
+
+    except KeyError as e:
+        click.echo(e)
+    except JSONDecodeError as e:
+        click.echo(e)
+
+
+def print_lists(list_words):
+    if isinstance(list_words, list):
+        click.echo(", ".join(x for x in list_words))
+
+
+def print_thesaurus(json_):
+    word = json_['results'][0]['word']
+    click.echo()
+    click.echo(word)
+    for i in json_["results"]:
+        for j in i["lexicalEntries"]:
+            click.echo()
+            click.echo(f"({j['lexicalCategory']})")  # noun/ verb etc
+            click.echo()
+            for k in j["entries"]:
+                for v in k["senses"]:
+                    thesaurus(v, 'synonyms')
+                    thesaurus(v, 'antonyms')
