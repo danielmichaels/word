213f064df138f7bd36109f9822b554b267af7259
Dan <dans.address@outlook.com>
2018-10-01 23:22:16 +0000

    Plumb in synonym & antonym method + tidy up parameters within class

--- word/word_main.py
+++ word/word_main.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 
+import click
 import requests
 
 from output import thesaurus_printer, definition
@@ -14,37 +15,34 @@ class Word:
         self.oxford_api_key = self.config['OXFORD']['OXFORD_API_KEY']
         self.oxford_app_id = self.config['OXFORD']['OXFORD_APP_ID']
         self.base_url = 'https://od-api.oxforddictionaries.com:443/api/v1'
+        self.entries_url = '/entries/en/'
+        self.thesaurus_url = '/synonyms;antonyms'
 
     def call_api(self, url):
         params = {"app_id": self.oxford_app_id, "app_key": self.oxford_api_key}
-        response = requests.get(url, headers=params)
-        return response.json()
+        try:
+            response = requests.get(url, headers=params)
+            return response.json()
+        except requests.exceptions.ConnectionError as e:
+            click.echo(f"Connection Error encountered! {e}")
 
     def definition(self, word):
-        url = self.base_url + '/entries/en/' + word.lower()
+        url = self.base_url + self.entries_url + word.lower()
         json_ = self.call_api(url)
         definition(json_, 'definitions')
 
     def thesaurus(self, word):
-        both = '/synonyms;antonyms'
-        url = self.base_url + '/entries/en/' + word.lower() + both
+        url = self.base_url + self.entries_url + word.lower() + self.thesaurus_url
+        json_ = self.call_api(url)
+        thesaurus_printer(json_, 'both')
+
+    def antonyms(self, word):
+        url = self.base_url + self.entries_url + word.lower() + self.thesaurus_url
+        json_ = self.call_api(url)
+        thesaurus_printer(json_, 'antonyms')
+
+    def synonyms(self, word):
+        url = self.base_url + self.entries_url + word.lower() + self.thesaurus_url
         json_ = self.call_api(url)
         thesaurus_printer(json_, 'synonyms')
-        # printer(json_, 'antonyms')
 
-    def antonyms(self):
-        pass
-
-    def synonyms(self):
-        pass
-
-    def run(self):
-        """Used prior to plumbing in click."""
-        pass
-
-
-w = Word()
-w.definition('gentrification')
-w.definition('ace')
-# w.thesaurus('house')
-# w.thesaurus('ace')
