4ef327c5a9908fe91dabd9dc5b3fb3f62da11ae7
Dan <dans.address@outlook.com>
2018-10-05 20:14:05 +0000

    Exceptions added for common types.

---
+++ word/exceptions.py
@@ -0,0 +1,34 @@
+class OxfordExceptions(Exception):
+    def __init__(self, status, msg):
+        self.status = status
+        self.msg = msg
+
+
+class UnauthorizedError(OxfordExceptions):
+    # 401
+    pass
+
+
+class AuthenticationError(OxfordExceptions):
+    # 403
+    pass
+
+
+class NotFoundError(OxfordExceptions):
+    # 404
+    pass
+
+
+class InternalServerError(OxfordExceptions):
+    # 500
+    pass
+
+
+class BadGatewayError(OxfordExceptions):
+    # 502
+    pass
+
+
+class ServiceUnavailableError(OxfordExceptions):
+    # 503
+    pass
--- word/word_main.py
+++ word/word_main.py
@@ -4,6 +4,8 @@ import os
 import click
 import requests
 
+from exceptions import UnauthorizedError, AuthenticationError, NotFoundError, \
+    InternalServerError, BadGatewayError, ServiceUnavailableError
 from output import thesaurus_printer, definition
 
 
@@ -22,6 +24,30 @@ class Word:
         params = {"app_id": self.oxford_app_id, "app_key": self.oxford_api_key}
         try:
             response = requests.get(url, headers=params)
+            if response.status_code != 200:
+                # set the exceptions here
+
+                if response.status_code == 401:
+                    raise UnauthorizedError(response.status_code,
+                                            'Unauthorized')
+                if response.status_code == 403:
+                    raise AuthenticationError(response.status_code,
+                                              'Authentication Error')
+                if response.status_code == 404:
+                    raise NotFoundError(response.status_code,
+                                        'Resource Not Found')
+                if response.status_code == 500:
+                    raise InternalServerError(response.status_code,
+                                              'Internal Server Error')
+
+                if response.status_code == 502:
+                    raise BadGatewayError(response.status_code,
+                                          'Bad Gateway')
+
+                if response.status_code == 503:
+                    raise ServiceUnavailableError(response.status_code,
+                                                  'Service Temporarily Unavailable')
+
             return response.json()
         except requests.exceptions.ConnectionError as e:
             click.echo(f"Connection Error encountered! {e}")
@@ -45,4 +71,3 @@ class Word:
         url = self.base_url + self.entries_url + word.lower() + self.thesaurus_url
         json_ = self.call_api(url)
         thesaurus_printer(json_, 'synonyms')
-
